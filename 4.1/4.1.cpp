c367098e54a8737ddc3e581c24c8e578d48c32fa
polikarpov13 <vladyslav.polikarpov.ik.2021@lpnu.ua>
2021-10-02 12:54:48 +0300

    master commit

---
+++ 4.1/4.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int k = 1;
+
+    double S = 0;
+
+    cout << "N = ";
+    cin >> N;
+
+    cout << endl;
+
+    int i = 1;
+    while (i <= N) {
+        S += (1 + sqrt(1 + pow(i, 2))) / pow(i, 2);
+        i++;
+    }
+
+    cout << "S = " << S << endl;
+
+    S = 0;
+    i = 1;
+
+    do {
+        S += (1 + sqrt(1 + pow(i, 2))) / pow(i, 2);
+        i++;
+
+    } while (i <= N);
+
+    cout << "S = " << S << endl;
+
+    S = 0;
+
+    for (i = 1; i <= N; i++) {
+        S += (1 + sqrt(1 + pow(i, 2))) / pow(i, 2);
+    }
+
+    cout << "S = " << S << endl;
+
+    S = 0;
+
+    for (i = N; i >= 1; i--) {
+        S += (1 + sqrt(1 + pow(i, 2))) / pow(i, 2);
+    }
+
+    cout << "S = " << S << endl;
+
+    system("pause");
+    return 0;
+}
